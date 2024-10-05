643346a7fe963a7be908e340dac92444c7deec1c
Hacker-HTML <83747144+Hacker-HTML@users.noreply.github.com>
2024-10-05 12:33:44 -0700

    Create edits.py

---
+++ edits.py
@@ -0,0 +1,17 @@
+# install through IDE terminal
+from openai import OpenAI
+client = OpenAI()
+
+response = client.images.edit((
+  model="dall-e-2",
+  image=open("sunlit_lounge.png", "rb"),
+
+  # Area where image should be edited
+  mask=open("mask.png", "rb"),
+
+  # Include full description in the prompt not just the edited part
+  prompt="A sunlit indoor lounge area with a pool containing a flamingo",
+  n=1,
+  size="1024x1024"
+)
+image_url = response.data[0].url
